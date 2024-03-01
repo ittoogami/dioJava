0e07dd14eba0bf082caf2fce427b14bf7fe70a9d
ittoogami <thiagoramosassuncao@gmail.com>
2024-02-29 21:36:10 -0300

    criaçao da classe CPF

--- src/com/java/util/Cpf.java
+++ src/com/java/util/Cpf.java
@@ -2,4 +2,15 @@ package com.java.util;
 
 public class Cpf {
 
+	public String cpf;
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	
 }
